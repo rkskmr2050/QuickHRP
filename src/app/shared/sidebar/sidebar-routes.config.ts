0a28a8c64f33fa3f93aba91c183e757e8d99d9be
Vijay Sharma <vijaysharma1003@outlook.com>
2024-09-15 18:03:19 +0530

    Updated Menu Icons.

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -2,8 +2,8 @@ import { RouteInfo } from './sidebar.metadata';
 
 //Sidebar menu Routes and data
 export const ROUTES: RouteInfo[] = [
-    { path: '/appointment', title: 'Appointment', icon: 'bx bx-envelope icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-    { path: '/opd', title: 'OPD', icon: 'bx bx-envelope icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-    { path: '/office-front', title: 'OfficeFront', icon: 'bx bx-envelope icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
-    { path: '/pharmacy', title: 'Pharmacy', icon: 'bx bx-envelope icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+    { path: '/appointment', title: 'Appointment', icon: 'lni lni-calendar icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+    { path: '/opd', title: 'OPD', icon: 'fadeIn animated bx bx-plus-medical icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+    { path: '/office-front', title: 'OfficeFront', icon: 'fadeIn animated bx bx-buildings icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
+    { path: '/pharmacy', title: 'Pharmacy', icon: 'fadeIn animated bx bx-first-aid icon-color-2', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
 ];
