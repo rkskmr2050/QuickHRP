cd3fbafa25fa53b98eadb23ec1fbe9ecb2c131d5
Vijay Sharma <vijaysharma1003@outlook.com>
2024-09-15 18:05:37 +0530

    Updated Page Title

--- src/app/office-front/office-front-routing.module.ts
+++ src/app/office-front/office-front-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
         path: '',
         component: OfficeFrontComponent,
         data: {
-          title: 'office-front'
+          title: 'OfficeFront'
         }
       },
     ]
--- src/app/pharmacy/pharmacy-routing.module.ts
+++ src/app/pharmacy/pharmacy-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
         path: '',
         component: PharmacyComponent,
         data: {
-          title: 'pharmacy'
+          title: 'Pharmacy'
         }
       },
     ]
